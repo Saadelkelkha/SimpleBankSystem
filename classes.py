397f8b89c9755863b51a5ce1ab8f834610379bde
Saad el kelkha <146880449+Saadelkelkha@users.noreply.github.com>
2023-11-29 22:56:43 +0100

    Add files via upload

---
+++ classes.py
@@ -0,0 +1,66 @@
+import random
+import csv
+class Agent() :
+    count = 0
+    Compte = {}
+    Client = {}
+    ClientCompte = {}
+
+    with open('CSVfile.csv', 'w') as fd :
+        csv_writer = csv.writer(fd)
+        csv_writer.writerow(['NumCl', 'NumC', 'Mpc', 'Balance']) 
+
+    def __init__(self):
+        pass
+
+    def GeneratorAccountNum(self) :
+        return str(Agent.count) + str(random.randint(0, 100))
+    
+    def GeneratorAccountMot(self) :
+        return str(random.randint(100000,999999) )
+
+    def AjouterCompte(self, Balance = 0):
+        Agent.count += 1
+        numcl = Agent.count
+        numc = self.GeneratorAccountNum()
+        mpc = self.GeneratorAccountMot()
+        Agent.Compte[numc] = Balance
+        Agent.Client[numcl] = mpc
+        Agent.ClientCompte[numc] = numcl
+        print("The account was created with the customer number: ", numcl ," and account number: ",numc," and the password: ",mpc)
+        with open('CSVfile.csv', 'w') as fd :
+            csv_writer = csv.writer(fd)
+            csv_writer.writerow([numcl, numc, mpc, Balance])
+
+    def SupprimerCompte(self,numc):
+        del Agent.Compte[numc]
+        del Agent.Client[Agent.ClientCompte[numc]]
+        del Agent.ClientCompte[numc]
+
+    def manipSTS(self):
+        numComptesListe = list(self.ClientCompte.values())
+        numComptesTuple = tuple(self.ClientCompte.values())
+        numComptesSet = set(self.ClientCompte.values())
+
+class client(Agent) :
+    def __init__(self, numcl, mpc):
+        self.numcl = numcl
+        self.mpc = mpc
+
+    def Deposer(self) :
+        arg = float(input("type number of money : "))
+        numc = str(input("type number account : "))
+        Agent.Compte[numc] += arg
+
+    def Retirer(self) :
+        while True :
+            arg = float(input("type number of money : "))
+            numc = str(input("type number account : "))
+            if Agent.Compte[numc] >= arg :
+                Agent.Compte[numc] = Agent.Compte[numc] - arg
+                break
+            else :
+                print("you don't have enough money              ")
+    
+    def modifierMPClient(self, NMP):
+        Agent.Client[self.numcl] = NMP
---
+++ main2.py
@@ -0,0 +1,93 @@
+from classes import Agent, client
+agent = Agent()
+while True:
+    print("Main Menu :")
+    print("1_ Bank officer")
+    print("2_ Bank customer")
+    print("3_ To leave")
+    
+    choixp = input("Choose an option (1/2/3): ")
+
+    if choixp == '1':
+        while True:
+            print("Bank Agent Menu : ")
+            print("1_ Add an account")
+            print("2_ Delete an account")
+            print("3_ Back")
+
+            choixag = input("Choose an option (1/2/3): ")
+
+            if choixag == '1':
+                agent.AjouterCompte()
+
+            elif choixag == '2':
+                numc_supprimer = input("Account number to delete : ")
+                agent.SupprimerCompte(numc_supprimer)
+                print("Account deleted successfully.")
+
+            elif choixag == '3':
+                break
+
+            else :
+                print("Invalid choice. Please choose a valid option.")
+
+    elif choixp == '2':
+        while True :
+            print("Bank Customer Menu : ")
+        
+            print("1. change the password")
+            print("2. Deposit money")
+            print("3. Withdraw money")
+            print("4. Back")
+
+            choixc = input("Choose an option (1/2/3/4) : ")
+
+            if choixc == '1':
+                while True :
+                    numcl = int(input("Type customer number : "))
+                    mpc = str(input("Type password :"))
+                    client1 = client(numcl, mpc)
+                    if (numcl in Agent.Client) and (Agent.Client[numcl] == mpc) :
+                        break 
+                    else :
+                        print("Account is not defined")
+                
+                nmp = str(input("Type new password : "))
+                client1.modifierMPClient(nmp)
+
+            elif choixc == '2':
+                while True :
+                    numcl = int(input("Type customer number : "))
+                    mpc = str(input("Type new password : "))
+                    client1 = client(numcl, mpc)
+                    if (numcl in Agent.Client) and (Agent.Client[numcl] == mpc) :
+                        break 
+                    else :
+                        print("Account is not defined")
+                client1.Deposer()
+                print("Deposit successfully completed.")
+
+            elif choixc == '3':
+                while True :
+                    numcl = int(input("Type customer number : "))
+                    mpc = str(input("Type new password : "))
+                    client1 = client(numcl, mpc)
+                    if (numcl in Agent.Client) and (Agent.Client[numcl] == mpc) :
+                        break 
+                    else :
+                        print("Account is not defined")
+                client1.Retirer()
+                print("Withdrawal completed successfully.")
+
+            elif choixc == '4':
+                break
+
+            else:
+                print("Invalid choice. Please choose a valid option.")
+
+    elif choixp == '3':
+        print("Bye!")
+        break
+
+    else:
+        print("Invalid choice. Please choose a valid option.")
