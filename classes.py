7039a6d9d67eec5032870e48b61e342483d78d71
Saad el kelkha <146880449+Saadelkelkha@users.noreply.github.com>
2023-11-30 21:59:00 +0100

    Update classes.py

--- classes.py
+++ classes.py
@@ -1,25 +1,31 @@
 import random
 import csv
-class Agent() :
+
+class Agent():
+    # Class variables to keep track of counts and relationships between clients, accounts, and balances.
     count = 0
     Compte = {}
     Client = {}
     ClientCompte = {}
 
-    with open('CSVfile.csv', 'w') as fd :
+    # Creating a CSV file and writing headers if it doesn't exist.
+    with open('CSVfile.csv', 'w') as fd:
         csv_writer = csv.writer(fd)
         csv_writer.writerow(['NumCl', 'NumC', 'Mpc', 'Balance']) 
 
     def __init__(self):
         pass
 
-    def GeneratorAccountNum(self) :
+    # Generates a unique account number combining the agent's count and a random number.
+    def GeneratorAccountNum(self):
         return str(Agent.count) + str(random.randint(0, 100))
     
-    def GeneratorAccountMot(self) :
-        return str(random.randint(100000,999999) )
+    # Generates a random account password.
+    def GeneratorAccountMot(self):
+        return str(random.randint(100000, 999999) )
 
-    def AjouterCompte(self, Balance = 0):
+    # Adds a new account with a unique account number, a corresponding client number, and a password.
+    def AjouterCompte(self, Balance=0):
         Agent.count += 1
         numcl = Agent.count
         numc = self.GeneratorAccountNum()
@@ -27,40 +33,48 @@ class Agent() :
         Agent.Compte[numc] = Balance
         Agent.Client[numcl] = mpc
         Agent.ClientCompte[numc] = numcl
-        print("The account was created with the customer number: ", numcl ," and account number: ",numc," and the password: ",mpc)
-        with open('CSVfile.csv', 'w') as fd :
+        print("The account was created with the customer number:", numcl, "and account number:", numc, "and the password:", mpc)
+        # Appends the account details to the CSV file.
+        with open('CSVfile.csv', 'a') as fd:
             csv_writer = csv.writer(fd)
             csv_writer.writerow([numcl, numc, mpc, Balance])
 
-    def SupprimerCompte(self,numc):
+    # Removes an account based on the account number.
+    def SupprimerCompte(self, numc):
         del Agent.Compte[numc]
         del Agent.Client[Agent.ClientCompte[numc]]
         del Agent.ClientCompte[numc]
 
+    # Manipulates sets derived from client and account relationships.
     def manipSTS(self):
         numComptesListe = list(self.ClientCompte.values())
         numComptesTuple = tuple(self.ClientCompte.values())
         numComptesSet = set(self.ClientCompte.values())
 
-class client(Agent) :
+class client(Agent):
+    # Initializes a client with a client number and password.
     def __init__(self, numcl, mpc):
         self.numcl = numcl
         self.mpc = mpc
 
-    def Deposer(self) :
-        arg = float(input("type number of money : "))
-        numc = str(input("type number account : "))
+    # Deposits money into a specified account.
+    def Deposer(self):
+        arg = float(input("type number of money: "))
+        numc = str(input("type number account: "))
         Agent.Compte[numc] += arg
 
-    def Retirer(self) :
-        while True :
-            arg = float(input("type number of money : "))
-            numc = str(input("type number account : "))
-            if Agent.Compte[numc] >= arg :
+    # Withdraws money from a specified account if there are sufficient funds.
+    def Retirer(self):
+        while True:
+            arg = float(input("type number of money: "))
+            numc = str(input("type number account: "))
+            if Agent.Compte[numc] >= arg:
                 Agent.Compte[numc] = Agent.Compte[numc] - arg
                 break
-            else :
-                print("you don't have enough money              ")
-    
+            else:
+                print("you don't have enough money")
+
+    # Modifies the password of the client.
     def modifierMPClient(self, NMP):
         Agent.Client[self.numcl] = NMP
+
